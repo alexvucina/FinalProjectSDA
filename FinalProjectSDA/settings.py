7b07948125fdbc0b004c2cbf5cfb03bb4d5af7f3
alexvucina <vucina.alexandru@gmail.com>
2022-10-24 18:18:25 +0300

    start student

--- FinalProjectSDA/settings.py
+++ FinalProjectSDA/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7ap1k6!5oxfd5v52r89rm6+p#kli@qfin4!ap=6ggymhq)hfbl'
+# SECRET_KEY = 'django-insecure-7ap1k6!5oxfd5v52r89rm6+p#kli@qfin4!ap=6ggymhq)hfbl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
