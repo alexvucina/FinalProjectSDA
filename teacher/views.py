72e36b4df3afb8e98193e9066ccec9161229086d
alexvucina <vucina.alexandru@gmail.com>
2022-11-02 20:19:25 +0200

    modify list of teachers

--- teacher/views.py
+++ teacher/views.py
@@ -17,26 +17,29 @@ class TeacherListView(ListView):
     template_name = 'teacher/list_of_teachers.html'
     model = Teacher
     context_object_name = 'all_teachers'
-    permission_required = 'teacher.view_list_of_teachers'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        all_teachers = Teacher.objects.all()
+        data['all_teachers'] = all_teachers
+
+        return data
 
 
 class TeacherUpdateView(UpdateView):
     template_name = 'teacher/update_teacher.html'
     model = Teacher
     form_class = TeacherForm
-    success_url = reverse_lazy('list_of_teachers')
-    permission_required = 'teacher.update_teacher'
+    success_url = reverse_lazy('list-of-teachers')
 
 
 class TeacherDeleteView(DeleteView):
     template_name = 'teacher/delete_teacher.html'
     model = Teacher
-    success_url = reverse_lazy('list_of_teachers')
-    permission_required = 'teacher.delete_teacher'
+    success_url = reverse_lazy('list-of-teachers')
 
 
 class TeacherDetailView(DetailView):
     template_name = 'teacher/details_teacher.html'
     model = Teacher
     permission_required = 'teacher.view_teacher_details'
-
